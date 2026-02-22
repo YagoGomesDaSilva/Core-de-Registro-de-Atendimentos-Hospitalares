f6dd8d4ed7de2e0f32d44ae1acaf6d1501e2b14f
YagoGomesDaSilva <yago.gomes.silva.ygs@gmail.com>
2026-02-22 17:38:35 -0300

    Padroniza nomes de tabelas e ajusta URL do IIS
    Padroniza os nomes das tabelas de "Pacientes" e "Atendimentos" para "Paciente" e "Atendimento" no script.sql, incluindo ajustes nas constraints e inserts. Adiciona comandos para remoção das tabelas antigas antes da criação das novas. Altera a URL do IIS no WebApplication.csproj para http://localhost:8080/.

--- script.sql
+++ script.sql
@@ -1,22 +1,22 @@
 -- Criação da Base de Dados
 CREATE DATABASE HospitalDB;
-GO
+
+--DROP TABLE Pacientes
+--DROP TABLE Atendimentos
 
 USE HospitalDB;
-GO
 
 -- Criação da Tabela de Pacientes
-CREATE TABLE Pacientes (
+CREATE TABLE Paciente (
     Id INT IDENTITY(1,1) PRIMARY KEY,
     Nome VARCHAR(150) NOT NULL,
     Cpf VARCHAR(11) NOT NULL UNIQUE,
     DataNascimento DATE NOT NULL,
     DataRegisto DATETIME DEFAULT GETDATE()
 );
-GO
 
--- Criação da Tabela de Atendimentos
-CREATE TABLE Atendimentos (
+-- Criação da Tabela de Atendimento
+CREATE TABLE Atendimento (
     Id INT IDENTITY(1,1) PRIMARY KEY,
     PacienteId INT NOT NULL,
     DataEntrada DATETIME NOT NULL DEFAULT GETDATE(),
@@ -24,20 +24,18 @@ CREATE TABLE Atendimentos (
     PressaoArterial VARCHAR(15) NULL,
     Temperatura DECIMAL(4,2) NULL,
     FrequenciaCardiaca INT NULL,
-    CONSTRAINT FK_Atendimento_Paciente FOREIGN KEY (PacienteId) REFERENCES Pacientes(Id)
+    CONSTRAINT FK_Atendimento_Paciente FOREIGN KEY (PacienteId) REFERENCES Paciente(Id)
 );
-GO
 
 -- Inserção de Dados Fictícios para Teste
-INSERT INTO Pacientes (Nome, Cpf, DataNascimento) 
+INSERT INTO Paciente (Nome, Cpf, DataNascimento) 
 VALUES ('João Silva', '12345678901', '1985-04-12');
 
-INSERT INTO Pacientes (Nome, Cpf, DataNascimento) 
+INSERT INTO Paciente (Nome, Cpf, DataNascimento) 
 VALUES ('Maria Santos', '10987654321', '1990-08-25');
 
-INSERT INTO Atendimentos (PacienteId, DataEntrada, StatusAtendimento, PressaoArterial, Temperatura, FrequenciaCardiaca)
+INSERT INTO Atendimento (PacienteId, DataEntrada, StatusAtendimento, PressaoArterial, Temperatura, FrequenciaCardiaca)
 VALUES (1, GETDATE(), 'Finalizado', '120/80', 36.5, 75);
 
-INSERT INTO Atendimentos (PacienteId, DataEntrada, StatusAtendimento, PressaoArterial, Temperatura, FrequenciaCardiaca)
+INSERT INTO Atendimento (PacienteId, DataEntrada, StatusAtendimento, PressaoArterial, Temperatura, FrequenciaCardiaca)
 VALUES (2, GETDATE(), 'Ativo', '130/85', 37.2, 82);
-GO
